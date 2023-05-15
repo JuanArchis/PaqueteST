5ddbc79c9b97c2deb1241c51d80efbf41ce84dad
Juan Camilo Ordoñez Deñas <119546440+JuanArchis@users.noreply.github.com>
2023-05-15 13:52:09 -0500

    Dependencias, correción

--- R/Dependencias.R
+++ R/Dependencias.R
@@ -4,7 +4,7 @@
 Librerias=function(Librerias_base){
   Librerias_base = c("readxl","dplyr","ggplot2","boot","broom","GGally","gridExtra","stargazer","rlang","testthat","trees","devtools","remotes") # Nombra las librerias necesarias
   
-  if (!require("pacman","remotes")) install.packages("pacman","remotes") # Paquete que simplifica la carga de librerias
+  if (!require("pacman")) install.packages("pacman")# Paquete que simplifica la carga de librerias
   pacman::p_load(char = Librerias_base)
   remotes::install_github("rstudio/learnr")
   remotes::install_github("rstudio/gradethis")
