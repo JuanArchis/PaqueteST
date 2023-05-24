221fe4338cd4fb4bb8f2d404d6f5f786d24dbbee
Juan Camilo Ordoñez Deñas <119546440+JuanArchis@users.noreply.github.com>
2023-05-24 14:34:38 -0500

    FUnción de librerias ver 0.0.2

--- R/Dependencias.R
+++ R/Dependencias.R
@@ -2,13 +2,16 @@
 
 
 Librerias=function(Librerias_base){
-  Librerias_base = c("readxl","dplyr","ggplot2","boot","broom","GGally","gridExtra","stargazer","rlang","testthat","trees","devtools","remotes") # Nombra las librerias necesarias
+  Librerias_base = c("readxl","dplyr","ggplot2","boot","broom","GGally","gridExtra","stargazer","rlang","testthat","devtools","remotes") # Nombra las librerias necesarias
   
   if (!require("pacman")) install.packages("pacman")# Paquete que simplifica la carga de librerias
   pacman::p_load(char = Librerias_base)
-  remotes::install_github("rstudio/learnr")
-  remotes::install_github("rstudio/gradethis")
+
+  remotes::install_github("rstudio/learnr",force=TRUE)
+  remotes::install_github("rstudio/gradethis",force=TRUE)
   
 }
+
+
 
 
